57b4ed31be1c5fb68095798bc5cfe466cbae40c9
RahulRSankolli <rahuldghj79@gmail.com>
2026-08-17 09:50:49 +0530

    Day 10: Added new form and form input handling

--- script.js
+++ script.js
@@ -23,3 +23,81 @@ console.log(heroTitle);
 if(heroTitle){
     heroTitle.textContent = `${getGreeting()}, I'm Rahul 👋`;
 }
+// ==== MOBILE MENU TOGGLE ===
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () =>{
+    navLinks.classList.toggle('open');
+    menuToggle.setAttribute('aria-expanded',navLinks.classList.contains('open'));
+});
+
+// === SCROLL-BASED NAVBAR STYLING ====
+const header = document.querySelector('.site-header');
+
+window.addEventListener('scroll', () =>{
+    if (window.scrollY > 50){
+        header.classList.add('scrolled')
+    }
+    else{
+        header.classList.remove('scrolled')
+    }
+})
+
+// === ACTIVE NAV LINK on scroll ====
+const sections = document.querySelectorAll('section[id]');
+const navItems = document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll', () =>{
+    let current ='';
+    sections.forEach(section =>{
+        if(window.scrollY >= section.offsetTop-100){
+            current=section.getAttribute('id');
+        }
+    })
+    navItems.forEach(link =>{
+        link.classList.remove('active');
+        if(link.getAttribute('href') === #${current}) {
+            link.classList.add('active');
+        }
+    })
+})
+
+/ === FORM & INPUT HANDLING ===
+const myForm = document.querySelector('#myForm');
+const userInput = document.querySelector('#userInput');
+
+if (myForm) {
+    myForm.addEventListener('submit', (e) => {
+        // 1. Prevent form submission (stops page refresh)
+        e.preventDefault();
+
+        // Get the current value from the input box
+        const inputValue = userInput.value;
+
+        // 2. Print input value in console
+        console.log("User Input:", inputValue);
+
+        // 3. Show alert on button click
+        alert(Entered Value: ${inputValue});
+    });
+}
+// === FORM & INPUT HANDLING ===
+const myForm = document.querySelector('#myForm');
+const userInput = document.querySelector('#userInput');
+
+if (myForm) {
+    myForm.addEventListener('submit', (e) => {
+        // 1. Prevent form submission (stops page refresh)
+        e.preventDefault();
+
+        // Get the current value from the input box
+        const inputValue = userInput.value;
+
+        // 2. Print input value in console
+        console.log("User Input:", inputValue);
+
+        // 3. Show alert on button click
+        alert(Entered Value: ${inputValue});
+    });
+}
